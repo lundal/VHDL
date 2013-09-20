a78afa1314af78dd21e53d6fd448591e338fd204
bj0rn <meister.bj0rn@gmail.com>
2013-09-20 13:46:28 +0200

    Fixes adder to be generic

--- FPGAToplevel/Adder.vhd
+++ FPGAToplevel/Adder.vhd
@@ -32,10 +32,11 @@ use IEEE.STD_LOGIC_SIGNED.ALL;
 --use UNISIM.VComponents.all;
 
 entity Adder is
+    generic (N : integer)
     Port (
-				A : in  STD_LOGIC_VECTOR(63 downto 0);
-				B : in  STD_LOGIC_VECTOR (63 downto 0);
-				res : out  STD_LOGIC_VECTOR (63 downto 0);
+				A : in  STD_LOGIC_VECTOR(N-1 downto 0);
+				B : in  STD_LOGIC_VECTOR (N-1 downto 0);
+				res : out  STD_LOGIC_VECTOR (N-1 downto 0);
 				overflow : out STD_LOGIC);
 end Adder;
 
@@ -43,7 +44,7 @@ architecture Behavioral of Adder is
 
 		Signal same_sign : STD_LOGIC;
 		Signal sign_check : STD_LOGIC;
-		Signal result : STD_LOGIC_VECTOR (63 downto 0);
+		Signal result : STD_LOGIC_VECTOR (N-1 downto 0);
 
 begin
 	ADD: process (A, B, same_sign, sign_check, result) is
@@ -52,9 +53,9 @@ begin
 		-- Overflow may only happen when both MSBs from A and Bare the same 
 		-- If both are same, then same_sign and sign_check will be set to '1'
 		-- Same_sign and sign_check are used to control overflow.
-		if (A(63) = B(63)) then
+		if (A(N-1) = B(N-1)) then
 			same_sign <= '1'; 
-			sign_check <= A(63);
+			sign_check <= A(N-1);
 		else
 			same_sign <='0';
 		end if;
@@ -66,7 +67,7 @@ begin
 		-- Result based on output res used for overflow control
 		--	result <= A + B;							
 		-- Change in MSB between result and old inputs means overflow (sich_check is a copy used for the control)
-			if(sign_check /= result(63)) then 
+			if(sign_check /= result(N-1)) then 
 				overflow <= '1';
 			else
 				overflow <= '0';
