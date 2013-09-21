adb83b82abaab345e9ac9122ab10fea6e1dc5f44
Per Thomas Lundal <perthomas@gmail.com>
2013-09-21 19:44:58 +0200

    Everything except multiplier is done. Untested

--- FPGAToplevel/ALU.vhd
+++ FPGAToplevel/ALU.vhd
@@ -20,51 +20,108 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+library WORK;
+use WORK.ALU_CONSTANTS.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- NOTE: UNTESTED!
 
 entity ALU is
 	
 	generic(N : integer := 64);
 	
-	type ALU_FUNC is STD_LOGIC_VECTOR(3 downto 0);
-	type ALU_DATA_BUS is STD_LOGIC_VECTOR(N-1 downto 0);
-	
-	type ALU_FLAGS is
-	record
-		Carry		:	STD_LOGIC;
-		Overflow	:	STD_LOGIC;
-		Zero		:	STD_LOGIC;
-		Negative	:	STD_LOGIC;
-	end record;
-	
-    port(
-		X		:	in	ALU_DATA_BUS;
-		Y		:	in	ALU_DATA_BUS;
-		R		:	out	ALU_DATA_BUS;
-		FLAGS	:	out	ALU_FLAGS
+	port(
+		X		:	in	STD_LOGIC_VECTOR(N-1 downto 0);
+		Y		:	in	STD_LOGIC_VECTOR(N-1 downto 0);
+		R		:	out	STD_LOGIC_VECTOR(N-1 downto 0);
+		FUNC	:	in	STD_LOGIC_VECTOR(3 downto 0);
+		FLAGS	:	out ALU_FLAGS
 	);
 	
 end ALU;
 
 architecture Behavioral of ALU is
 	
+	component Adder is
+		generic (N : integer := 64);
+		port (
+			A			:	in	STD_LOGIC_VECTOR(N-1 downto 0);
+			B			:	in	STD_LOGIC_VECTOR(N-1 downto 0);
+			R			:	out	STD_LOGIC_VECTOR(N-1 downto 0);
+			CARRY_IN	:	in	STD_LOGIC;
+			OVERFLOW	:	out	STD_LOGIC
+		);
+	end component;
+	
+	-- Adder signals
+	signal add_a		:	STD_LOGIC_VECTOR(N-1 downto 0);
+	signal add_b		:	STD_LOGIC_VECTOR(N-1 downto 0);
+	signal add_carry_in	:	STD_LOGIC;
+	signal add_overflow	:	STD_LOGIC;
+	
 	-- Result signals
-	signal R_ADD	:	ALU_DATA_BUS;
-	signal R_SUB	:	ALU_DATA_BUS;
-	signal R_MUL	:	ALU_DATA_BUS;
-	signal R_AND	:	ALU_DATA_BUS;
-	signal R_OR		:	ALU_DATA_BUS;
-	signal R_XOR	:	ALU_DATA_BUS;
+	signal r_addsub	:	STD_LOGIC_VECTOR(N-1 downto 0);
+	--signal r_mul	:	STD_LOGIC_VECTOR(N-1 downto 0);
+	signal r_and	:	STD_LOGIC_VECTOR(N-1 downto 0);
+	signal r_or		:	STD_LOGIC_VECTOR(N-1 downto 0);
+	signal r_xor	:	STD_LOGIC_VECTOR(N-1 downto 0);
+	
+	-- Other signals
+	signal y_new	:	STD_LOGIC_VECTOR(N-1 downto 0);
+	signal result	:	STD_LOGIC_VECTOR(N-1 downto 0);
 	
 begin
-
-
+	
+	ADDER_SUBTRACTOR : Adder
+	generic map (N => N)
+	port map(
+		A			=> X,
+		B			=> y_new,
+		R			=> r_addsub,
+		CARRY_IN	=> add_carry_in,
+		OVERFLOW	=> add_overflow
+	);
+	
+	-- TODO: Multiplier!
+	
+	INVERT_Y : process(Y, FUNC)
+	begin
+		if FUNC = ALU_FUNC_SUB then
+			y_new <= not Y;
+			add_carry_in <= '1';
+		else
+			y_new <= Y;
+			add_carry_in <= '0';
+		end if;		
+	end process INVERT_Y;
+	
+	LOGICS : process(X, Y, FUNC)
+	begin
+		r_and		<=	X and Y;
+		r_or		<=	X or Y;
+		r_xor		<=	X xor Y;
+	end process LOGICS;
+	
+	--RESULTIFIER : process(FUNC, r_addsub, r_mul, r_and, r_or, r_xor)
+	RESULTIFIER : process(FUNC, r_addsub, r_and, r_or, r_xor)
+	begin
+		case FUNC is
+			when ALU_FUNC_ADD	=>	result <= r_addsub;
+			when ALU_FUNC_SUB	=>	result <= r_addsub;
+			--when ALU_FUNC_MUL	=>	result <= r_mul;
+			when ALU_FUNC_AND	=>	result <= r_and;
+			when ALU_FUNC_OR	=>	result <= r_or;
+			when ALU_FUNC_XOR	=>	result <= r_xor;
+			when others			=>	
+		end case;
+	end process RESULTIFIER;
+	
+	-- TODO: Overflow!
+	
+	FLAGS.Positive	<= '1' when not (result = ZERO64) and result(N-1) = '0' else '0';
+	FLAGS.Zero		<= '1' when result = ZERO64 else '0';
+	FLAGS.Negative	<= '1' when result(N-1) = '1' else '0';
+	
+	R <= result;
+	
 end Behavioral;
 
---
+++ FPGAToplevel/ALU_CONSTANTS.vhd
@@ -0,0 +1,36 @@
+--	Package File Template
+--
+--	Purpose: This package defines supplemental types, subtypes, 
+--		 constants, and functions 
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+package ALU_CONSTANTS is
+	
+	constant ALU_FUNC_WIDTH	:	integer	:=	4;
+	
+	-- ALU Functions
+	constant ALU_FUNC_ADD	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0000";
+	constant ALU_FUNC_SUB	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0001";
+	constant ALU_FUNC_MUL	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0010";
+	constant ALU_FUNC_SRA	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0011";
+	constant ALU_FUNC_OR	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0100";
+	constant ALU_FUNC_AND	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0101";
+	constant ALU_FUNC_XOR	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0110";
+	constant ALU_FUNC_SLI	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0111";
+	constant ALU_FUNC_SRL	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"1000";
+	
+	type ALU_FLAGS is
+	record
+		Positive	:	STD_LOGIC;
+		Zero		:	STD_LOGIC;
+		Negative	:	STD_LOGIC;
+		Overflow	:	STD_LOGIC;
+	end record;
+	
+	constant ZERO64	: STD_LOGIC_VECTOR(63 downto 0) :=  (63 downto 0 => '0');
+	constant ONE64	: STD_LOGIC_VECTOR(63 downto 0) :=  (63 downto 0 => '0');
+	
+end ALU_CONSTANTS;
