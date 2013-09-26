7ef27186e0d64548fc08941cec150b818d90e6c8
Per Thomas Lundal <perthomas@gmail.com>
2013-09-26 16:00:17 +0200

    Fix typo

--- FPGAToplevel/ALU_CONSTANTS.vhd
+++ FPGAToplevel/ALU_CONSTANTS.vhd
@@ -19,7 +19,7 @@ package ALU_CONSTANTS is
 	constant ALU_FUNC_OR	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0100";
 	constant ALU_FUNC_AND	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0101";
 	constant ALU_FUNC_XOR	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0110";
-	constant ALU_FUNC_SLI	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0111";
+	constant ALU_FUNC_SLL	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0111";
 	constant ALU_FUNC_SRL	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"1000";
 	
 	type ALU_FLAGS is
@@ -30,7 +30,9 @@ package ALU_CONSTANTS is
 		Overflow	:	STD_LOGIC;
 	end record;
 	
+	constant ZERO32	: STD_LOGIC_VECTOR(31 downto 0) :=  (31 downto 0 => '0');
 	constant ZERO64	: STD_LOGIC_VECTOR(63 downto 0) :=  (63 downto 0 => '0');
-	constant ONE64	: STD_LOGIC_VECTOR(63 downto 0) :=  (63 downto 0 => '0');
+	constant ONE32	: STD_LOGIC_VECTOR(31 downto 0) :=  (31 downto 0 => '1');
+	constant ONE64	: STD_LOGIC_VECTOR(63 downto 0) :=  (63 downto 0 => '1');
 	
 end ALU_CONSTANTS;
