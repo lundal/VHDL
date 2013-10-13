85eac19df21fe734dc205628bd220d31233ee1db
bj0rn <meister.bj0rn@gmail.com>
2013-10-13 16:44:58 +0200

    Decode stage is now synthesizeble
    - Ready for further testing

--- FPGAToplevel/decode_stage.vhd
+++ FPGAToplevel/decode_stage.vhd
@@ -45,11 +45,11 @@ entity decode_stage is
           read_data1            : out STD_LOGIC_VECTOR(63 downto 0);
           read_data2            : out STD_LOGIC_VECTOR(63 downto 0);
           immediate_value_out   : out STD_LOGIC_VECTOR(31 downto 0);
-          immediate_address_out : out STD_LOGIC_VECTOR(31 downto 0)
+          immediate_address_out : out STD_LOGIC_VECTOR(31 downto 0);
           rs                    : out STD_LOGIC_VECTOR(4 downto 0);
           rt                    : out STD_LOGIC_VECTOR(4 downto 0);
           rd                    : out STD_LOGIC_VECTOR(4 downto 0);
-          );
+          condition_out         : out STD_LOGIC_VECTOR(3 downto 0));
           
 end decode_stage;
 
@@ -65,17 +65,16 @@ component register_file
           RD_ADDR            : in  STD_LOGIC_VECTOR(RADDR_BUS-1 downto 0);
           WRITE_DATA         : in  STD_LOGIC_VECTOR(DDATA_BUS-1 downto 0);
           RS                 : out STD_LOGIC_VECTOR(DDATA_BUS-1 downto 0);
-          RT                 : out STD_LOGIC_VECTOR(DDATA_BUS-1 downto 0);
-          condition_out      : out STD_LOGIC_VECTOR(2 downto 0));
+          RT                 : out STD_LOGIC_VECTOR(DDATA_BUS-1 downto 0));
 end component;
 
 -- Signal declerations
-signal rd_addr           : std_logic_vector(4 downto 0);
-signal rs_addr           : std_logic_vector(4 downto 0);
-signal rd_addr           : std_logic_vector(4 downto 0);
-signal immediate_value   : std_logic_vector(9 downto 0);
-signal immediate_address : std_logic_vector(18 downto 0);
-signal condition         : std_logic_vector(2 downto 0);
+signal rd_addr_internal                 : std_logic_vector(4 downto 0);
+signal rs_addr_internal                 : std_logic_vector(4 downto 0);
+signal rt_addr_internal                 : std_logic_vector(4 downto 0);
+signal immediate_value_internal         : std_logic_vector(9 downto 0);
+signal immediate_address_internal       : std_logic_vector(18 downto 0);
+signal condition_internal               : std_logic_vector(3 downto 0);
 
 
 
@@ -85,8 +84,8 @@ REGISTER_FILE_MAP : register_file
 port map (CLK => clk, 
           RESET => reset, 
           RW => reg_write, 
-          RS_ADDR => rs_addr, 
-          RT_ADDR => rt_addr, 
+          RS_ADDR => rs_addr_internal, 
+          RT_ADDR => rt_addr_internal, 
           RD_ADDR => write_register, 
           WRITE_DATA => write_data, 
           RS => read_data1, 
@@ -96,41 +95,41 @@ port map (CLK => clk,
 FETCH : process(processor_enable, instruction) 
 begin 
     if processor_enable = '1' then
-       rd_addr <= instruction(23 downto 19);
-       rs_addr <= instruction(18 downto 14);
-       rt_addr <= instruction(13 downto 9);
-       immediate_value <= instruction(13 downto 4)
-       immediate_address <= instruction(18 downto 0)
-       condition <= instruction(31 downto 28);
+       rd_addr_internal <= instruction(23 downto 19);
+       rs_addr_internal <= instruction(18 downto 14);
+       rt_addr_internal <= instruction(13 downto 9);
+       immediate_value_internal <= instruction(13 downto 4);
+       immediate_address_internal <= instruction(18 downto 0);
+       condition_internal <= instruction(31 downto 28);
      
      elsif processor_enable = '0' then
-        rd_addr  <= (others => '0');
-        rs_addr <= (others => '0');
-        rt_addr <= (others => '0');
-        immediate_value <= (others => '0');
-        imediate_address <= (others => '0');
-        condition <= (others => '0');
+        rd_addr_internal  <= (others => '0');
+        rs_addr_internal <= (others => '0');
+        rt_addr_internal <= (others => '0');
+        immediate_value_internal <= (others => '0');
+        immediate_address_internal <= (others => '0');
+        condition_internal <= (others => '0');
      end if;
 end process FETCH;
 
 
-SIGN_EXTEND_IMMEDIATE_VALUE : process(immediate_value) 
+SIGN_EXTEND_IMMEDIATE_VALUE : process(immediate_value_internal) 
 begin 
-     immediate_value_out <= SXT(immediate_value, 32);   
+     immediate_value_out <= SXT(immediate_value_internal, 32);   
     
 end process SIGN_EXTEND_IMMEDIATE_VALUE;
 
 
-SIGN_EXTEND_IMMEDIATE_ADDRESS : process(immediate_address)
+SIGN_EXTEND_IMMEDIATE_ADDRESS : process(immediate_address_internal)
 begin 
-    immediate_address_out <= SXT(immediate_address, 32);
+    immediate_address_out <= SXT(immediate_address_internal, 32);
 end process SIGN_EXTEND_IMMEDIATE_ADDRESS;
 
 --OUTPUT
-condition_out <= condition;
-rs <= rs_addr;
-rt <= rt_addr;
-rd <= rd_addr;
+condition_out <= condition_internal;
+rs <= rs_addr_internal;
+rt <= rt_addr_internal;
+rd <= rd_addr_internal;
 
 
 
