d26ff0d2873dd1093d5c0a3ca7c1194cda9f9142
bj0rn <meister.bj0rn@gmail.com>
2013-10-13 17:46:10 +0200

    Started on mapping the execution stage

--- FPGAToplevel/execution_stage.vhd
+++ FPGAToplevel/execution_stage.vhd
@@ -20,6 +20,9 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+library WORK;
+use WORK.ALU_CONSTANTS.ALL;
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
@@ -30,26 +33,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity execution_stage is
-    port (clk             : in STD_LOGIC;
-          reset           : in STD_LOGIC;
+    port (clk               : in STD_LOGIC;
+          reset             : in STD_LOGIC;
           
           -- Control signals
-          reg_dst         : in STD_LOGIC;
-          alu_src         : in STD_LOGIC;
-          alu_op          : in STD_LOGIC_vector(1 downto 0);
+          reg_dst           : in STD_LOGIC;
+          alu_src           : in STD_LOGIC;
+          alu_op            : in STD_LOGIC_vector(1 downto 0);
           
           --Signals in 
-          pc_incremented  : in STD_LOGIC_VECTOR(31 downto 0);
-          rs              : in STD_LOGIC_VECTOR(31 downto 0);
-          rt              : in STD_LOGIC_VECTOR(31 downto 0);
-          immediate       : in STD_LOGIC_VECTOR(31 downto 0);
-          rs_addr         : in STD_LOGIC_VECTOR(4 downto 0);
-          rt_addr         : in STD_LOGIC_VECTOR(4 downto 0);
+          pc_incremented    : in STD_LOGIC_VECTOR(31 downto 0);
+          read_data1        : in STD_LOGIC_VECTOR(63 downto 0);
+          read_data2        : in STD_LOGIC_VECTOR(63 downto 0);
+          immediate_value   : in STD_LOGIC_VECTOR (63 downto 0);
+          immediate_address : in STD_LOGIC_VECTOR (31 downto 0);
+          immediate         : in STD_LOGIC_VECTOR(31 downto 0);
+          rs_addr           : in STD_LOGIC_VECTOR(4 downto 0);
+          rt_addr           : in STD_LOGIC_VECTOR(4 downto 0);
+          rd_addr           : in STD_LOGIC_VECTOR(4 downto 0);
+          
+          -- From other stages
+          stage3_alu_result : in STD_LOGIC_VECTOR(63 downto 0);
+          stage4_write_data : in STD_LOGIC_VECTOR(63 downto 0);
           
           -- Signals out 
-          write_register  : out STD_LOGIC_VECTOR(31 downto 0);
-          alu_result      : out STD_LOGIC_VECTOR(31 downto 0);
-          pc_adder_result : out STD_LOGIC_VECTOR (31 downto 0));
+          write_register    : out STD_LOGIC_VECTOR(31 downto 0);
+          alu_result        : out STD_LOGIC_VECTOR(31 downto 0);
+          addr_adder_result   : out STD_LOGIC_VECTOR (31 downto 0));
 end execution_stage;
 
 architecture Behavioral of execution_stage is
@@ -63,7 +73,16 @@ component ALU
 		R		:	out	STD_LOGIC_VECTOR(N-1 downto 0);
 		FUNC	:	in	STD_LOGIC_VECTOR(3 downto 0);
 		FLAGS	:	out ALU_FLAGS);
-	
+end component ALU; 	
+
+component tri_multiplexor
+    generic (N : NATURAL);
+    Port ( sel : in  STD_LOGIC_VECTOR (1 downto 0);
+           in0 : in  STD_LOGIC_VECTOR (N-1 downto 0);
+           in1 : in  STD_LOGIC_VECTOR (N-1 downto 0);
+           in2 : in  STD_LOGIC_VECTOR (N-1 downto 0);
+           output  : in  STD_LOGIC_VECTOR (N-1 downto 0));
+end component;
 
 component multiplexor
     generic (N : NATURAL);
@@ -83,46 +102,84 @@ component Adder
 	);
 end component;
 
+component alu_control_unit
+    Port ( alu_op  : in  STD_LOGIC_VECTOR (1 downto 0);
+           function_code : in  STD_LOGIC_VECTOR (5 downto 0);
+           alu_ctrl_out : in  STD_LOGIC_VECTOR (3 downto 0));
+end component;
+
+
+component forwarding_unit 
+    Port ( rs : in  STD_LOGIC_VECTOR (4 downto 0);
+           rt : in  STD_LOGIC_VECTOR (4 downto 0);
+           stage3_reg_rd : in  STD_LOGIC_VECTOR (4 downto 0);
+           stage4_reg_rd : in  STD_LOGIC_VECTOR (4 downto 0);
+           stage3_reg_write : in  STD_LOGIC;
+           stage4_reg_write : in  STD_LOGIC;
+           forwardA : out  STD_LOGIC_VECTOR (1 downto 0);
+           forwardB : out  STD_LOGIC_VECTOR (1 downto 0));
+end component;
+
+
+
 
 -- Signal declerations
- 
+
+signal tri_mux1_out : STD_LOGIC_VECTOR(63 downto 0);
+signal tri_mux2_out : STD_LOGIC_VECTOR(63 downto 0);
+signal alu_op2      : STD_LOGIC_VECTOR(63 downto 0);
+
+--Control signals
+signal forwardA : STD_LOGIC_VECTOR(1 downto 0);
+signal forwardB : STD_LOGIC_VECTOR(1 downto 0); 
+
 
 
 begin
 
 -- Mappings
 
-PC_ADDR_ADDER_MAP : Adder 
-    generic map(N => 32)
-    port map(A =>, 
-             B =>, 
-             R =>, 
-             CARRY_IN =>, 
-             OVERFLOW =>);
+
+
+
+TRI_MUX1_MAP : tri_multiplexor
+generic map(N => 64)
+port map(sel => forwardA, 
+         in0 => read_data1, 
+         in1 => stage4_write_data, 
+         in2 => stage3_alu_result, 
+         output => tri_mux1_out);
          
+         
+TRI_MUX2_MAP : tri_multiplexor
+generic map(N => 64)
+port map(sel => forwardB, 
+         in0 => read_data2, 
+         in1 => stage4_write_data, 
+         in2 => stage3_alu_result,
+         output => tri_mux2_out);
+
+ALU_MUX_MAP : multiplexor
+generic map(N => 64)
+port map(sel => alu_src, 
+         in0 => tri_mux2_out, 
+         in1 => immediate_value, 
+         output => alu_op2);
+
 ALU_MAP : ALU 
-generic MAP(N => 64)
-port map( X =>, 
-          Y =>, 
-          R =>, 
-          FUNC =>, 
-          FLAGS =>,  
-)
-
-
-MUX_ALU_MAP : multiplexor 
-generic map( N => )
-port map (sel =>, 
-          in0 =>, 
-          in1 =>, 
-          output);
+generic map(N => 64)
+port map( X => tri_mux1_out, 
+          Y =>alu_op2, 
+          R => alu_result,
+          FUNC =>,
+          FLAGS =>);
           
-MUX_WRITE_REGISTER_MAP : multiplexor 
-generic map(N => 5)
-port map( sel =>, 
-          in0 =>, 
-          in1 =>, 
-          output =>); 
+
+
+--Processes
+
+
+
 
 end Behavioral;
 
