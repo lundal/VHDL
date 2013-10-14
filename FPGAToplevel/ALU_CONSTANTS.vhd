178616ec93588c22577d48d440d878ef630b22a9
Per Thomas Lundal <perthomas@gmail.com>
2013-10-14 12:11:01 +0200

    Add condition constants

--- FPGAToplevel/ALU_CONSTANTS.vhd
+++ FPGAToplevel/ALU_CONSTANTS.vhd
@@ -10,6 +10,7 @@ use IEEE.STD_LOGIC_1164.all;
 package ALU_CONSTANTS is
 	
 	constant ALU_FUNC_WIDTH	:	integer	:=	4;
+	constant COND_WIDTH		:	integer	:=	4;
 	
 	-- ALU Functions
 	constant ALU_FUNC_ADD	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0000";
@@ -22,6 +23,18 @@ package ALU_CONSTANTS is
 	constant ALU_FUNC_SLL	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"0111";
 	constant ALU_FUNC_SRL	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"1000";
 	constant ALU_FUNC_NA	:	STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0)	:=	"1111";
+	
+	-- Condition codes
+	constant COND_NEVER		:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "0000";
+	constant COND_EQUAL		:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "0001";
+	constant COND_NEQUAL	:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "0010";
+	constant COND_GREATEREQ	:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "0011";
+	constant COND_GREATER	:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "0100";
+	constant COND_LESSEQ	:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "0101";
+	constant COND_LESS		:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "0110";
+	constant COND_OWERFLOW	:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "0111";
+	constant COND_NOWERFLOW	:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "1000";
+	constant COND_ALWAYS	:	STD_LOGIC_VECTOR(COND_WIDTH-1 downto 0) := "1111";
 	
 	type ALU_FLAGS is
 	record
