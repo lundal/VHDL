baf2d9a472ef88e7d8ccfdb2509654cef4e25027
bj0rn <meister.bj0rn@gmail.com>
2013-10-14 11:45:45 +0200

    Renames register names

--- FPGAToplevel/forwarding_unit.vhd
+++ FPGAToplevel/forwarding_unit.vhd
@@ -32,10 +32,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity forwarding_unit is
     Port ( rs : in  STD_LOGIC_VECTOR (4 downto 0);
            rt : in  STD_LOGIC_VECTOR (4 downto 0);
-           stage3_reg_rd : in  STD_LOGIC_VECTOR (4 downto 0);
            stage4_reg_rd : in  STD_LOGIC_VECTOR (4 downto 0);
-           stage3_reg_write : in  STD_LOGIC;
+           stage5_reg_rd : in  STD_LOGIC_VECTOR (4 downto 0);
            stage4_reg_write : in  STD_LOGIC;
+           stage5_reg_write : in  STD_LOGIC;
            forwardA : out  STD_LOGIC_VECTOR (1 downto 0);
            forwardB : out  STD_LOGIC_VECTOR (1 downto 0));
 end forwarding_unit;
@@ -48,11 +48,11 @@ begin
 FORWARD_UNIT : process (rs, rt, stage3_reg_rd, stage4_reg_rd, stage3_reg_write, stage4_reg_write)
     begin 
         -- MEM hazard
-        if (stage4_reg_write = '1' 
-            and (stage4_reg_rd /= "00000")
-            and not (stage3_reg_write = '1' and (stage3_reg_rd /= "00000"))
-                and (stage3_reg_rd /= rs)
-            and (stage4_reg_rd = rs)) then 
+        if (stage5_reg_write = '1' 
+            and (stage5_reg_rd /= "00000")
+            and not (stage4_reg_write = '1' and (stage4_reg_rd /= "00000"))
+                and (stage4_reg_rd /= rs)
+            and (stage5_reg_rd = rs)) then 
             
             forwardA <= "01";
             
@@ -62,10 +62,10 @@ FORWARD_UNIT : process (rs, rt, stage3_reg_rd, stage4_reg_rd, stage3_reg_write, 
         
         
         if (stage4_reg_write = '1' 
-            and (stage4_reg_rd /= "00000")
-            and not (stage3_reg_write = '1' and (stage3_reg_rd /= "00000"))
-                and (stage3_reg_rd = rt)
-            and (stage4_reg_rd = rt)) then 
+            and (stage5_reg_rd /= "00000")
+            and not (stage4_reg_write = '1' and (stage4_reg_rd /= "00000"))
+                and (stage4_reg_rd = rt)
+            and (stage5_reg_rd = rt)) then 
             
             forwardB <= "01";
         else 
@@ -74,16 +74,16 @@ FORWARD_UNIT : process (rs, rt, stage3_reg_rd, stage4_reg_rd, stage3_reg_write, 
             
             
        --EX hazard 
-        if (stage3_reg_write = '1'
-            and (stage3_reg_rd /= "0000")
-            and (stage3_reg_rd = rs)) then 
+        if (stage4_reg_write = '1'
+            and (stage4_reg_rd /= "0000")
+            and (stage4_reg_rd = rs)) then 
             
             forwardA <= "10";
         end if;
         
-        if (stage3_reg_write = '1'
-            and (stage3_reg_rd /= "00000")
-            and (stage3_reg_rd = rt)) then 
+        if (stage4_reg_write = '1'
+            and (stage4_reg_rd /= "00000")
+            and (stage4_reg_rd = rt)) then 
              
             forwardB <= "10";
         end if;
