3a75da605c4dc2ea1405e75ecc36769fd06b286f
bj0rn <meister.bj0rn@gmail.com>
2013-10-18 13:46:34 +0200

    Adds template for the control unit
    - It is based on the notes
    - Incomplete (Will cause errors)
    
    DO NOT MIND THE ERRORS FOR NOW

--- FPGAToplevel/control_unit.vhd
+++ FPGAToplevel/control_unit.vhd
@@ -27,11 +27,165 @@ use WORK.CONSTANTS.ALL;
 entity control_unit is
 	port (
 		OP_CODE		:	in	STD_LOGIC_VECTOR(OP_CODE_WIDTH-1 downto 0);
-		ALU_SOURCE	:	out	STD_LOGIC;
+		FUNC        :   in  STD_LOGIC_VECTOR(ALU_FUNC_WIDHT-1 downto 0);
+        ALU_SOURCE	:	out	STD_LOGIC;
 		IMM_SOURCE	:	out STD_LOGIC;
-		MEM_WRITE	:	out	STD_LOGIC;
-		REG_WRITE	:	out	STD_LOGIC;
-		MEM_TO_REG	:	out	STD_LOGIC;
-		REG_DEST	:	out	STD_LOGIC
+		REG_SOURCE  :   out STD_LOGIC;
+        REG_WRITE	:	out	STD_LOGIC;
+        CALL        :   out STD_LOGIC;
+        JUMP        :   out STD_LOGIC;
+        ALU_FUNC    :   out STD_LOGIC_VECTOR(ALU_FUNC_WIDTH-1 downto 0);
+        GENE_OP     :   out STD_LOGIC_VECTOR(GENE_OP_WIDTH-1 downto 0);
+        MEM_OP      :   out STD_LOGIC_VECTOR(MEM_OP_WIDTH-1 downto 0);
+        TO_REG      :   out STD_LOGIC_VECTOR(TO_REG_OP_WIDTH-1 downto 0);
 	);
 end entity;
+
+architecture Behavioral of control_unit is
+
+begin
+
+CONTROL_UNIT : process (OP_CODE)
+    begin 
+        case OP_CODE is 
+        when RRR =>
+            ALU_FUNC <= FUNC;
+            REG_SOURCE <= '0';
+            IMM_SOURCE <= '0';'
+            ALU_SOURCE <= '0';
+            GENE_OP <= NOP; 
+            MEM_OP <= NOP;
+            JUMP <= '0';
+            CALL <= '0';
+            TO_REG <= "001";
+            REG_WRITE <= '1';
+            
+        when RRI =>
+            ALU_FUNC <= FUNC;
+            REG_SOURCE <= '0';
+            IMM_SOURCE <= '0';'
+            ALU_SOURCE <= '1';
+            GENE_OP <= NOP; 
+            MEM_OP <= NOP;
+            JUMP <= '0';
+            CALL <= '0';
+            TO_REG <= "001";
+            REG_WRITE <= '1';
+        
+        when CALL =>
+            ALU_FUNC <= ADD;
+            REG_SOURCE <= '1';
+            IMM_SOURCE <= '1';'
+            ALU_SOURCE <= '1';
+            GENE_OP <= NOP; 
+            MEM_OP <= NOP;
+            JUMP <= '1';
+            CALL <= '1';
+            TO_REG <= "010";
+            REG_WRITE <= '1';
+            
+        when JUMP => 
+            ALU_FUNC <= ADD;
+            REG_SOURCE <= '1';
+            IMM_SOURCE <= '1';'
+            ALU_SOURCE <= '1';
+            GENE_OP <= NOP; 
+            MEM_OP <= NOP;
+            JUMP <= '1';
+            CALL <= '0';
+            TO_REG <= NOP;
+            REG_WRITE <= '0';
+        
+        when LOAD =>
+            ALU_FUNC <= ADD;
+            REG_SOURCE <= '0';
+            IMM_SOURCE <= '0';'
+            ALU_SOURCE <= '1';
+            GENE_OP <= NOP; 
+            MEM_OP <= LOAD_DATA;
+            JUMP <= '0';
+            CALL <= '0';
+            TO_REG <= "011";
+            REG_WRITE <= '1';
+        
+        when LOAD_IMM =>
+            ALU_FUNC <= ADD;
+            REG_SOURCE <= '0';
+            IMM_SOURCE <= '1';'
+            ALU_SOURCE <= '1';
+            GENE_OP <= NOP; 
+            MEM_OP <= LOAD_DATA;
+            JUMP <= '0';
+            CALL <= '0';
+            TO_REG <= "011";
+            REG_WRITE <= '1';
+
+        when STORE =>
+            ALU_FUNC <= ADD;
+            REG_SOURCE <= '0';
+            IMM_SOURCE <= '0';'
+            ALU_SOURCE <= '1';
+            GENE_OP <= NOP; 
+            MEM_OP <= STORE_DATA;
+            JUMP <= '0';
+            CALL <= '0';
+            TO_REG <= NOP;
+            REG_WRITE <= '0';
+        
+        when STORE_IMM =>
+            ALU_FUNC <= FUNC;
+            REG_SOURCE <= '0';
+            IMM_SOURCE <= '1';'
+            ALU_SOURCE <= '1';
+            GENE_OP <= NOP; 
+            MEM_OP <= STORE_DATA;
+            JUMP <= '0';
+            CALL <= '0';
+            TO_REG <= NOP;
+            REG_WRITE <= '1';
+        
+        when LOAD_GENE =>
+            ALU_FUNC <= NOP;
+            REG_SOURCE <= '0';
+            IMM_SOURCE <= '0';'
+            ALU_SOURCE <= '0';
+            GENE_OP <= LOAD_GENE; 
+            MEM_OP <= NOP;
+            JUMP <= '0';
+            CALL <= '0';
+            TO_REG <= "000";
+            REG_WRITE <= '1';
+        
+        when STORE_GENE =>
+            ALU_FUNC <= NOP;
+            REG_SOURCE <= '0';
+            IMM_SOURCE <= '0';'
+            ALU_SOURCE <= '0';
+            GENE_OP <= NOP; 
+            MEM_OP <= NOP;
+            JUMP <= '0';
+            CALL <= '0';
+            TO_REG <= "000";
+            REG_WRITE <= '1';
+        
+        when SET_GENE_OPS =>
+            ALU_FUNC <= FUNC;
+            REG_SOURCE <= '0';
+            IMM_SOURCE <= '0';'
+            ALU_SOURCE <= '0';
+            GENE_OP <= NOP; 
+            MEM_OP <= NOP;
+            JUMP <= '0';
+            CALL <= '0';
+            TO_REG <= "001";
+            REG_WRITE <= '1';
+        
+        
+            
+        end case;
+end process CONTROL_UNIT;
+
+
+end Behavioral;
+
+
