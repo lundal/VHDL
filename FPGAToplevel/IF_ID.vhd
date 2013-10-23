d4de04fc3e6b91106510e684342a1c68f80f0383
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-10-23 19:43:45 +0200

    Refactor IF_ID, now looks like the spec drawing

--- FPGAToplevel/IF_ID.vhd
+++ FPGAToplevel/IF_ID.vhd
@@ -1,61 +1,37 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    10:35:50 09/30/2013 
--- Design Name: 
--- Module Name:    IF_ID - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use work.constants.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity IF_ID is
-    generic(N : integer := 32);
-    Port ( clk       : in  STD_LOGIC;
-           reset     : in  STD_LOGIC;
-           enable    : in  STD_LOGIC;
-           data_in1  : in  STD_LOGIC_VECTOR (N-1 downto 0);
-           data_in2  : in  STD_LOGIC_VECTOR (N-1 downto 0);
+    Port ( 
+           -- bit signals
+           clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           halt : in  STD_LOGIC;
+           
+           -- bus in
+           instruction_in  : in  STD_LOGIC_VECTOR (INST_WIDTH-1 downto 0);
            pc_incremented_in : in std_logic_vector(INST_WIDTH-1 downto 0);
            
-           data_out1 : out STD_LOGIC_VECTOR(N-1 downto 0);
-           data_out2 : out STD_LOGIC_VECTOR(N-1 downto 0);
-           pc_incremented_out : out std_logic_vector(INST_WIDTH-1 downto 0)
+           -- bus out
+           pc_incremented_out : out STD_LOGIC_VECTOR(INST_WIDTH-1 downto 0);
+           instruction_out : out STD_LOGIC_VECTOR(INST_WIDTH-1 downto 0);
            
            );
 end IF_ID;
 
-architecture Behavioral of IF_ID is
 
+architecture behavioral of IF_ID is
 
 --Component declerations
 component flip_flop 
     generic(N : NATURAL);
-    Port ( clk      : in std_logic;
-           reset    : in std_logic;
-           enable   : in std_logic;
-           data_in  : in std_logic_vector(N-1 downto 0);
-           data_out : out std_logic_vector(N-1 downto 0)
+    Port ( clk : in std_logic;
+           reset : in std_logic;
+           enable : in std_logic;
+           data_in : in std_logic_vector(INST_WIDTH-1 downto 0);
+           data_out : out std_logic_vector(INST_WIDTH-1 downto 0)
     );
 end component flip_flop;
 
@@ -64,24 +40,22 @@ begin
 
 --Mappings
 INCREMENTED_REGISTER : flip_flop
-generic map(N => 32)
+generic map(N => INST_WIDTH)
 port map (clk => clk,
           reset => reset,
-          enable => enable, 
-          data_in => data_in1, 
-          data_out => data_out1
+          enable => halt, 
+          data_in => pc_incremented_in, 
+          data_out => pc_incremented_out
 );
 
 
 INSTRUCTION_REGISTER : flip_flop 
-generic map(N => 32)
+generic map(N => INST_WIDTH)
 port map (clk => clk, 
           reset => reset, 
-          enable => enable,
-          data_in => data_in2,
-          data_out => data_out2
+          enable => halt,
+          data_in => instruction_in,
+          data_out => instruction_out
 );
 
-
-end Behavioral;
-
+end behavioral;
