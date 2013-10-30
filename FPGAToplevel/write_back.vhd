7ef81422a442fcf11e365c35723017f1383139ff
bj0rn <meister.bj0rn@gmail.com>
2013-10-30 22:51:08 +0100

    Adds skeleton for write_back stage

--- FPGAToplevel/write_back.vhd
+++ FPGAToplevel/write_back.vhd
@@ -30,8 +30,21 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity write_back is
-    Port ( read_data : in  STD_LOGIC_VECTOR (31 downto 0);
-           alu_result : in  STD_LOGIC_VECTOR (31 downto 0));
+    Port ( 
+				--Control signals in 
+				to_reg : in std_logic_vector(TO_REG_OP_WIDTH-1 downto 0);
+				call   : in std_logic;
+				
+				--Bus signals in 
+				gene_in : in std_logic_vector(DATA_WIDTH-1 downto 0);
+				res_in  : in std_logic_vector(DATA_WIDTH-1 downto 0);
+				data_in : in std_logic_vector(DATA_WIDTH-1 downto 0);
+				RDA_in 	: in std_logic_vector(REG_ADDR_WIDTH-1 downto 0);
+				
+				-- Bus signals out
+				WBA 		: out std_logic_vector();
+				WBB 		: out std_logic_vector()
+				
 end write_back;
 
 architecture Behavioral of write_back is
