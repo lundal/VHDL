f3449351c8659263697d14f9b5b1ae70cf3cf635
Per Thomas Lundal <perthomas@gmail.com>
2013-11-05 15:03:55 +0100

    Update PRNG testbench

--- FPGAToplevel/PRNG_tb.vhd
+++ FPGAToplevel/PRNG_tb.vhd
@@ -10,91 +10,47 @@ ARCHITECTURE behavior OF PRNG_tb IS
     -- Component Declaration for the Unit Under Test (UUT)
  
     COMPONENT PRNG
-    PORT(
-         clk : IN  std_logic;
-         reset : IN  std_logic;
-         load : IN  std_logic;
-         seed : IN  std_logic_vector(31 downto 0);
-         rnd_out : OUT  std_logic_vector(31 downto 0)
-        );
+    generic (
+        WIDTH : integer := 32
+    );
+    port (
+       RANDOM : out STD_LOGIC_VECTOR(WIDTH-1 downto 0);
+       CLK    : in  STD_LOGIC
+    );
     END COMPONENT;
     
 
    --Inputs
    signal clk : std_logic := '0';
-   signal reset : std_logic := '0';
-   signal load : std_logic := '0';
-   signal seed : std_logic_vector(31 downto 0) := (others => '0');
 
  	--Outputs
-   signal rnd_out : std_logic_vector(31 downto 0);
+   signal random : std_logic_vector(31 downto 0);
 
    -- Clock period definitions
    constant clk_period : time := 10 ns;
  
 BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: PRNG PORT MAP (
-          clk => clk,
-          reset => reset,
-          load => load,
-          seed => seed,
-          rnd_out => rnd_out
-        );
 
-   -- Clock process definitions
-   clk_process :process
-   begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
-   end process;
- 
+    -- Instantiate the Unit Under Test (UUT)
+    uut: PRNG PORT MAP (
+        random => random,
+        clk => clk
+    );
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      -- hold reset state for 100 ns.
-      wait for 100 ns;	
+    -- Clock process definitions
+    clk_process :process
+    begin
+        clk <= '0';
+        wait for clk_period/2;
+        clk <= '1';
+        wait for clk_period/2;
+    end process;
 
-      wait for clk_period*10;
-      reset <= '0';
-      wait for clk_period*5;
-      reset <= '1';
 
-      -- insert stimulus here 
-		
-		--Random number generator should be produsing different outputs at each clock cyccle
-		
-		wait for clk_period*5;
-		reset<='0';
-		
-		wait for clk_period*4;
-		
-		--Activating load signal
-		load<='1';
-		
-		wait for clk_period*4;
-		
-		--Dectivating load signal and resetting
-		load<='0';
-		reset<='1';
-		
-		wait for clk_period*2;
-		
-		--Activating load signal and havind a seed-value
-		reset<='0';
-		seed<= "00001010000010100000101000001010";
-		load<='1';
-		
-		wait for clk_period*4;
-		
-		--Deactivating load, keep pouring out random generated outputs
-		load<='0';
-
-      wait;
-   end process;
-
+    -- Stimulus process
+    stim_proc: process
+    begin		
+        --Random number generator should be produsing different outputs at each clock cyccle
+        wait;
+    end process;
 END;
