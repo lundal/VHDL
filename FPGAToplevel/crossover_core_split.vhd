016d0bea78d0df747e444b629a9a6ff9df890eed
Per Thomas Lundal <perthomas@gmail.com>
2013-11-06 18:19:04 +0100

    Apply shifting to split

--- FPGAToplevel/crossover_core_split.vhd
+++ FPGAToplevel/crossover_core_split.vhd
@@ -17,12 +17,41 @@ entity crossover_core_split is
 end crossover_core_split;
 
 architecture Behavioral of crossover_core_split is
-	signal split : INTEGER := 0; 
+    component ShifterVariable is
+        generic (
+            N : natural := 64;
+            M : natural := 6
+        );
+        port (
+            I		:	in	STD_LOGIC_VECTOR(N-1 downto 0);
+            O		:	out	STD_LOGIC_VECTOR(N-1 downto 0);
+            Left	:	in	STD_LOGIC;
+            Arith	:	in	STD_LOGIC;
+            Count	:	in	STD_LOGIC_VECTOR(M-1 downto 0)
+        );
+    end component;
+    
+	signal shifted : STD_LOGIC_VECTOR(N-1 downto 0);
+    signal mask1 : STD_LOGIC_VECTOR(N-1 downto 0);
+    signal mask2 : STD_LOGIC_VECTOR(N-1 downto 0);
 begin
+    SHIFTER : ShifterVariable
+    generic map (
+        N => N,
+        M => R
+    )
+    port map (
+        I     => (others => '1'),
+        O     => shifted,
+        Left  => '1',
+        Arith => '0',
+        Count => random_number
+    );
     
-    split <= TO_INTEGER(UNSIGNED(random_number));
+    mask1 <= shifted(N-2 downto 0) & '0';
+    mask2 <= not mask1;
     
-    child1 <= parent1(N-1 downto split+1) & parent2(split downto 0);
-    child2 <= parent2(N-1 downto split+1) & parent1(split downto 0);
+    child1 <= (parent1 and mask2) or (parent2 and mask1);
+    child2 <= (parent1 and mask1) or (parent2 and mask2);
 	
 end Behavioral;
